7e59a20dd087b57db68c1eee020906737f2c9e24
DavideDaniel <davidedaniel@gmail.com>
2016-02-04 03:25:46 -0800

    onload fix

--- js/onload.js
+++ js/onload.js
@@ -3,9 +3,8 @@ $(document)
     // sliders
     $('.sampleWork')
       .slick({
-        infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        mobileFirst: true
       });
 
     $('.testimonials')
