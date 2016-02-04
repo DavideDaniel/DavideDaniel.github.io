9e7fbf0f72aaa62f4534e8da40d2ea72304879ab
DavideDaniel <davidedaniel@gmail.com>
2016-02-04 01:59:28 -0800

    Feb 2016 ready

--- js/onload.js
+++ js/onload.js
@@ -1,82 +1,75 @@
 $(document)
-    .ready(function () {
-        // sliders
-        $('.sampleWork')
-            .slick({
-                autoplay: true,
-                autoplaySpeed: 3000,
-                centerMode: true,
-                centerPadding: '60px',
-                draggable: true,
-                dotsClass: 'slick-dots',
-                cssEase: 'ease',
-                easing: 'linear',
-                speed: 500,
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                responsive: [{
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                }]
-            });
-        $('.testimonials')
-            .slick({
-                autoplay: true,
-                autoplaySpeed: 3000,
-                centerMode: true,
-                centerPadding: '60px',
-                dots: true,
-                dotsClass: 'slick-dots',
-                draggable: true,
-                cssEase: 'ease',
-                easing: 'linear',
-                speed: 300,
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                responsive: [{
-                    breakpoint: 800,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                }]
-            });
+  .ready(function () {
+    // sliders
+    $('.sampleWork')
+      .slick({
+        slidesToShow: 3,
+        mobileFirst: true
+      });
 
-        $('h2.line-around')
-            .each(function (index, heading) {
+    $('.testimonials')
+      .slick({
+        initialSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        centerMode: true,
+        centerPadding: '50px',
+        dots: true,
+        dotsClass: 'slick-dots',
+        cssEase: 'ease',
+        easing: 'linear',
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [{
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }, {
+          breakpoint: 400,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
+      });
 
-                headingText = $(heading)
-                    .text()
+    $('h2.line-around')
+      .each(function (index, heading) {
 
-                var myFullText = '<span>' + headingText + '</span>';
-                $(heading)
-                    .html(myFullText);
-            })
+        headingText = $(heading)
+          .text()
 
-        $('#pdf-holder')
-            .bind('show', function (e) {
-                paneID = $(e.target)
-                    .attr('href');
-                src = $(paneID)
-                    .attr('data-src');
-                // if the iframe hasn't already been loaded once
-                if ($(paneID + " iframe")
-                    .attr("src") == "") {
-                    $(paneID + " iframe")
-                        .attr("src", src);
-                }
-            });
+        var myFullText = '<span>' + headingText + '</span>';
+        $(heading)
+          .html(myFullText);
+      })
 
-        $('a.pdf-btn')
-            .on('click', function (e) {
-                e.preventDefault();
+    $('#pdf-holder')
+      .bind('show', function (e) {
+        paneID = $(e.target)
+          .attr('href');
+        src = $(paneID)
+          .attr('data-src');
+        // if the iframe hasn't already been loaded once
+        if ($(paneID + " iframe")
+          .attr("src") == "") {
+          $(paneID + " iframe")
+            .attr("src", src);
+        }
+      });
 
-                $("#pdf-holder")
-                    .html('<iframe src="img/David_Daniel_Resume_2015_for_web.pdf" frameborder="0" style="overflow:auto;overflow-x:auto;overflow-y:auto;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" ></iframe>');
+    $('a.pdf-btn')
+      .on('click', function (e) {
+        e.preventDefault();
 
-            });
+        $("#pdf-holder")
+          .html(
+            '<iframe src="img/David_Daniel_Resume_Feb_2016.pdf" frameborder="0" style="overflow:auto;overflow-x:auto;overflow-y:auto;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" ></iframe>'
+          );
 
-    });
+      });
+
+  });
