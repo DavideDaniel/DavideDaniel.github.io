8bd98a91542359c9f77bcec1228a87abc644e543
DavideDaniel <davidedaniel@gmail.com>
2016-02-04 03:16:38 -0800

    slick s

--- js/onload.js
+++ js/onload.js
@@ -9,6 +9,11 @@ $(document)
         slidesToShow: 3,
         slidesToScroll: 3,
         responsive: [{
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          },
           breakpoint: 600,
           settings: {
             slidesToShow: 1,
