40892cab1142f197d042d58d1799691672d1cbd0
DavideDaniel <davidedaniel@gmail.com>
2016-02-04 02:36:26 -0800

    responsive fix

--- js/onload.js
+++ js/onload.js
@@ -10,11 +10,17 @@ $(document)
         draggable: true,
         dotsClass: 'slick-dots',
         cssEase: 'ease',
-        speed: 1000,
+        speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [{
-          breakpoint: 600,
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }, {
+          breakpoint: 400,
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1
