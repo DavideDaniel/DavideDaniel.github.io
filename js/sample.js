55ab834bf9ee41edadc201ba90ab34dc5e4e6538
DavideDaniel <davidedaniel@gmail.com>
2016-02-04 02:03:53 -0800

    Feb 2016 ready

--- js/sample.js
+++ js/sample.js
@@ -1,39 +1,36 @@
-"use strict";
+;
+(function ($) {
+  "use strict";
 
-// just a little bit of vanilla js fun... <3
+  // just a little bit of vanilla js fun... <3
 
-var hello = function (name, array) {
+  var hello = function (name, array) {
     if (name !== null) {
-        if (name === "World" || name === "world") {
-            name = name.toUpperCase();
-            alertify.alert("HELLO " + name + "!");
-        }
-        else {
-            array.push(name);
-            alertify.alert("oh, hi " + name + "...");
-            alertify.log("Visitors so far: " + array);
-        }
+      if (name === "World" || name === "world") {
+        name = name.toUpperCase();
+        alertify.alert("HELLO " + name + "!");
+      } else {
+        array.push(name);
+        alertify.alert("oh, hi " + name + "...");
+        alertify.log("Visitors so far: " + array);
+      }
     }
-};
+  };
 
+  document.addEventListener("DOMContentLoaded", function () {
+    var promptButton = document.querySelector("#promptButton");
+    var nameArray = [];
 
-    
-document.addEventListener("DOMContentLoaded", function() {
-var promptButton = document.querySelector("#promptButton");
-var nameArray = [];
-
-promptButton.addEventListener("click", function () {
-    alertify.prompt("Hi.. What's your name?", function (entry, string) {
+    promptButton.addEventListener("click", function () {
+      alertify.prompt("Hi.. What's your name?", function (entry,
+        string) {
         if (entry) {
-            hello(string, nameArray);
+          hello(string, nameArray);
         }
+      });
     });
-});
 
-})
+  })
 
-
-
-
-
-// have fun!
+  // have fun!
+})(jQuery);
