74b9fccef6c1a34dc4e4fdde729a9ec7d18d14e8
DavideDaniel <davidedaniel@gmail.com>
2016-02-04 02:54:49 -0800

    slickchange

--- js/onload.js
+++ js/onload.js
@@ -3,19 +3,19 @@ $(document)
     // sliders
     $('.sampleWork')
       .slick({
-        draggable: true,
-        dots: true,
-        dotsClass: 'slick-dots',
+        initialSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 3000,
         cssEase: 'ease',
         easing: 'linear',
         speed: 500,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         responsive: [{
           breakpoint: 600,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToScroll: 1
           }
         }, {
           breakpoint: 480,
