f86c8e66b6c37ea152be147c776f755d893a7602
DavideDaniel <davidedaniel@gmail.com>
2016-02-04 03:18:59 -0800

    infinite

--- js/onload.js
+++ js/onload.js
@@ -3,23 +3,9 @@ $(document)
     // sliders
     $('.sampleWork')
       .slick({
-        initialSlide: 0,
-        autoplay: true,
-        autoplaySpeed: 3000,
+        infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [{
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          },
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }]
+        slidesToScroll: 3
       });
 
     $('.testimonials')
